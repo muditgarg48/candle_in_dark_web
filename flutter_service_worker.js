01981d42667fcab00fb5326d2e7ef7746214db56
Mudit Garg <muditgarg48@gmail.com>
2024-02-23 14:28:04 +0000

    Adding feedback view ability to admin

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,7 +16,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ccaac40a7b20738fd50857095a29d0ff
 "assets/assets/json/faculty.json": "a0190aa598e16dcebd3beb3e0c920430",
 "assets/assets/json/imp_links.json": "3def5a56297ff55e255b4aef4a2f0dfa",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "540436d3b6082dfbf89063af2abfcb0c",
+"assets/fonts/MaterialIcons-Regular.otf": "ac76e74109b13dada08894d7358e12b2",
 "assets/NOTICES": "2f97f8903ef0c6e777f649624a368edf",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "fdd989f02576ec05830911556d83473a",
 "assets/packages/currency_picker/lib/src/res/no_flag.png": "3f454777dfe9b6aae5e9d8544f4fa6f6",
@@ -35,9 +35,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "ccaac40a7b20738fd50857095a29d0ff
 "icons/Icon-512.png": "6324c029ce41c00a672853289c5dba9c",
 "icons/Icon-maskable-192.png": "c29116ae59a293d89e07f8324b9d2d41",
 "icons/Icon-maskable-512.png": "6324c029ce41c00a672853289c5dba9c",
-"index.html": "93cad94fa9ddd3fec1dc11197b803e29",
-"/": "93cad94fa9ddd3fec1dc11197b803e29",
-"main.dart.js": "59463682b5912aeec2542355d0e27ec8",
+"index.html": "6c0473b974014902745bdd6fb71d398b",
+"/": "6c0473b974014902745bdd6fb71d398b",
+"main.dart.js": "117b205270ed1998eee448dc79583033",
 "manifest.json": "5afc310f14ab8dcddd578b49f18add1e",
 "version.json": "25963cae42c6034c443f05b4349a5fad"};
 // The application shell files that are downloaded before a service worker can
